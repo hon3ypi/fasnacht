3beb78d40a62d721d86a2e63bb17a251fcec4c58
hon3ypi <wanda.lao@gmail.com>
2020-11-11 13:22:42 +0100

    sass im commute entfernt

--- src/components/Commute.vue
+++ src/components/Commute.vue
@@ -24,7 +24,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style scoped>
 
 .person {
   color: rgb(103, 189, 189);
