c9e8b60c99ebe439f32f527774e4da0e6724e68a
Helena <mariahelena.brantschen@stud.hslu.ch>
2020-11-18 22:28:48 +0100

    Update Grend2.vue

--- src/views/Grend2.vue
+++ src/views/Grend2.vue
@@ -2,8 +2,48 @@
   <div class="grend2">
     <h1>Grend 2</h1>
 
-    DO CHOND S HTML INE
-  </div>
+ <section class="grend">
+        <section class="vid-wrapper">
+            <video class="xl-vid" src=""></video>
+        </section>
+        <p class="dialog">Dä, wo do verbi gloffe esch!</p>
+        <img class="" src="">
+        <p class="dialog">Do nomol!</p>
+        <span class="dialog-wrap dialog"> <span class="">Das esch jo en cooli </span>
+            <span class="nono"> Larva!</span></span>
+        <span class=""> esch</span>
+        <span class=""> Tabu!</span>
+
+        <div class="nono-words">
+            <p class="nono">Waggis!</p>
+            <p class="nono">Cortège!!</p>
+            <p class="nono">Schnitzelbängg!</p>
+            <p class="nono">Morgenschtraich!!</p>
+        </div>
+
+        <div class="layer"></div>
+        <img class="" src="">
+        <p class="dialog">Dä, wo do verbi gloffe esch!</p>
+        <p class="dialog">Ha grad es Déjà Vue.</p>
+        <p class="dialog">Das esch jo en coolE Grend!</p>
+
+        <div class="vid-wrapper">
+
+            <video src="" class="xl-vid"></video>
+            <h4>Guugge </h4>
+            <p>Rathaustreppe</p>
+
+            <video src="" class="s-vid"></video>
+            <p class="">Die Treppe ist stark belebt und regelmässig finden Aufführungen statt. Highlight für Guggenfans.
+            </p>
+            <video src="" class="m-vid"></video>
+            <video src="" class="m-vid"></video>
+            <video src="" class="s-vid"></video>
+
+
+        </div>
+
+    </section>  </div>
 </template>
 
 -->DAS ESCH FÖR S CSS
