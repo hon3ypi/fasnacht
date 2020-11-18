62c9bc5af53a2df16161950bd5ec3158c6ed56a7
Helena <mariahelena.brantschen@stud.hslu.ch>
2020-11-18 22:07:06 +0100

    tes
    test

--- src/views/Grend1.vue
+++ src/views/Grend1.vue
@@ -1,7 +1,40 @@
 <template>
   <div class="grend1">
     <h1>Fritschi</h1>
-    DO CHOND S HTML INE
+
+   <section class="fritschi">
+    <p class="">DU hast Bruder Fritschi gewählt</p>
+    <h4 class="">Bruder Fritschi</h4>
+    <img src="">
+
+
+   <span class="para-wrapper">
+    <h5>Gute Wahl</h5>
+<p class=""> Unübersehbar. Mit ehrfurchtgebietender gefurchter Holzmaske, langem Bart und im blauweissen Luzerner Staatsmantel. Einige sehen in ihm die mythische Symbolfigur eines heidnischen Fruchtbarkeitskults. Wahrscheinlicher ist, dass sein Vorbild ein «trinkfreudiger» Luzerner Landsknecht namens Fridolin (Friedrich, Fritschi) ist, der in der Schlacht von Ragaz 1446 am 6. März (Fridolinstag) beim Sieg der Eidgenossen über die Österreicher mitkämpfte.
+    Heute eröffnet er mit der Fritschifamilie am Schmudo die Fasnacht. Folge ihm. Er wird dir Orte zeigen, die du noch nicht kennst.  </p>
+</span>
+</section> 
+
+<section class="tagwach">
+
+<h4 class="">tagwach</h4>
+<div class="wrapper-video">
+    <h6 class="">Kapellplatz, SchmuDO 5 Uhr</h6>
+<video src="" class="xl-vid"></video>
+</div>
+<p class="">brüele</p>
+<p class="">Urknall</p>
+
+<video src="" class="m-vid"></video>
+<video src="" class="s-vid"></video>
+<video src="" class="s-vid"></video>
+<video src="" class="s-vid"></video>
+
+<p class="">1-0-0- t-a-u-s-e-n-d-e- P-a-p-i-e-r- s-c-h-n-i-t-z-e-l z-e-r-s-c-h-n-i-t-t-e-n-e T-e-l-e-f-o-n-b-ü-c-h-e-r-n- r-e-g-n-e-t-e-s-a-n-d-e-r-T-a-g-w-a-c-h-e-a-u-f-d-e-n-K-a-p-e-l-l-p-l-a-t-z-h-i-n-u-n-t-e-r-.
+</p>
+<img class ="" src="">
+
+</section>
   </div>
 </template>
 
--- src/views/Grend3.vue
+++ src/views/Grend3.vue
@@ -1,8 +1,122 @@
 <template>
   <div class="grend3">
     <h1>Huereaff</h1>
-    DO CHOND S HTML INE
-  </div>
+    <section class="huerenaff">
+    <section class="dialog-huerenaff">
+        <img src="" class="">
+        <p class="dialog">Hey Huerenaff Wie gohts?</p>
+        <p class="dialog">Danke guet ond der Huerenaff?</p>
+        <p class="dialog">Los Mou Huerenaff. Gömmer es Kafi Huerenaff
+            go näh?</p>
+        <p class="dialog">Jo Huerenaff. Das esch e rüüdig güeti Idee. </p>
+    </section>
+    <section class="drink-wrapper">
+
+        <div class="drink">
+            <p class="drinkname">Kafi Huerenaff</p>
+            <img class="" src="">
+            <p class="drinkcontent">starken Kaffee mit Apfel- oder Birnenträsch</p>
+        </div>
+
+        <div class="drink">
+            <p class="drinkname">Holdrio</p>
+            <img class="" src="">
+            <p class="drinkcontent">Hagebuttetee mit Zwetschenschnaps</p>
+        </div>
+
+        <div class="drink">
+            <p class="drinkname">«Mönze-Zwätschge»</p>
+            <img class="" src="">
+            <p class="drinkcontent">Pfefferminztee mit zwetschenschnaps</p>
+        </div>
+
+        <div class="drink">
+            <p class="drinkname">Häxetee</p>
+            <img class="" src="">
+            <p class="drinkcontent">Pfefferminztee mit Apfel oder Birneträsch</p>
+        </div>
+    </section>
+
+        <section class="abeleerete">
+            <div class="wrap-it">
+                <p class="dialog">“Gärn es Tee,eins mit Schnaps, ich bin ja nicht krank.”</p>
+                <img src="" class="">
+            </div>
+
+            <div class="wrap-it">
+                <p class="dialog">""</p>
+                <img src="" class="">
+            </div>
+
+            <div class="wrap-it">
+                <p class="dialog">“Mis Holdrio esch Schletzfertig!”</p>
+                <img src="" class="">
+            </div>
+
+            <div class="wrap-it">
+                <p class="dialog">“Mis Holdrio esch fasch fertig!”</p>
+                <img src="" class="">
+            </div>
+            <div class="wrap-it">
+                <p class="dialog">“”</p>
+                <img src="" class="">
+            </div>
+            <div class="wrap-it">
+                <p class="dialog">“Mis Holdrio esch trochulär!”</p>
+                <img src="" class="">
+            </div>
+        </section>
+        <section class="usgang">
+        <video class="m-vid" src=""></video>
+        <video class="m-vid" src=""></video>
+
+        
+            <div class="wrapper-video">
+
+            <video class="xl-vid" src=""></video>
+<div class="drinkscore"> 
+    <img src="" class="">
+    <img src="" class="">
+    <img src="" class="">
+
+    <img src="" class="">
+    <img src="" class="">
+    <img src="" class="">
+
+    <img src="" class="">
+    <img src="" class="">
+    <img src="" class="">
+</div>
+</div>
+
+    <video class="s-vid" src=""></video>
+    <video class="s-vid" src=""></video>
+    <video class="m-vid" src=""></video>
+
+</section> 
+
+<section class="monstercorso">
+<h4 class="">Monstercorso</h4>
+<video src="" class="s-vid"></video>
+<video src="" class="m-vid"></video>
+<video src="" class="s-vid"></video>
+<video src="" class="xs-vid"></video>
+
+<video src="" class="s-vid"></video>
+<video src="" class="s-vid"></video>
+
+</section>
+
+<section class="ende">
+    <p class="" >ende </p> 
+<span class="names">
+    <p class="" >Wanda </p> <p class="" >Michelle </p> <p class="" >Helena </p> 
+
+</span>
+</section>
+
+        
+</section>  </div>
 </template>
 
 -->DAS ESCH FÖR S CSS
