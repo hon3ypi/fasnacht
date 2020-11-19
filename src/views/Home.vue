806eccfc2f04978fd4058efbcf75f6ac30f62e8b
Helena <mariahelena.brantschen@stud.hslu.ch>
2020-11-19 12:54:05 +0100

    gridhome

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template class="">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
-<div class="home">
-      <div class="boxen">
+<div class="home boxen">
     <div class="box1"></div>
     <div class="box2"></div>
     <div class="box3"></div>
@@ -15,12 +14,11 @@
     <div class="box10"></div>
     <div class="box11"></div>
     <div class="box12"></div>
-  </div>
-    <p class="titel">Luzerner Fasnchat</p>
-  </div>
-    <p class="titel">Luzerner Fasnchat</p>
+        <p class="titel">Luzerner Fasnchat</p>
 
   </div>
+  
+
 
 </template>
 
@@ -32,28 +30,44 @@ html{
 
 .boxen { 
 display: grid; 
+border: 1px solid red;
 grid-template-columns: repeat(12, 1fr); 
 grid-template-rows: repeat(12, 1fr); 
 grid-column-gap: 0px;
 grid-row-gap: 0px; 
 }
-.box1 { grid-area: 1 / 1 / 4 / 5; } 
-.box2 { grid-area: 1 / 5 / 4 / 9; } 
-.box3 { grid-area: 1 / 9 / 4 / 13; } 
-.box4 { grid-area: 4 / 1 / 7 / 5; } 
-.box5 { grid-area: 4 / 5 / 7 / 9; } 
-.box6 { grid-area: 4 / 9 / 7 / 13; } 
-.box7 { grid-area: 7 / 1 / 10 / 5; } 
-.box8 { grid-area: 7 / 5 / 10 / 9; } 
-.box9 { grid-area: 7 / 9 / 10 / 13; } 
-.box10 { grid-area: 10 / 1 / 13 / 5; } 
-.box11 { grid-area: 10 / 5 / 13 / 9; } 
-.box12 { grid-area: 10 / 9 / 13 / 13; } 
+.box1 { grid-area: 1 / 1 / 4 / 5; 
+border: 1px solid red;
+background-color: aqua;
+} 
+.box2 { grid-area: 1 / 5 / 4 / 9; border: 1px solid red;
+} 
+.box3 { grid-area: 1 / 9 / 4 / 13;
+border: 1px solid red;
+ } 
+.box4 { grid-area: 4 / 1 / 7 / 5; border: 1px solid red;
+} 
+.box5 { grid-area: 4 / 5 / 7 / 9; border: 1px solid red;
+} 
+.box6 { grid-area: 4 / 9 / 7 / 13; border: 1px solid red;
+} 
+.box7 { grid-area: 7 / 1 / 10 / 5;border: 1px solid red;
+ } 
+.box8 { grid-area: 7 / 5 / 10 / 9;border: 1px solid red;
+ } 
+.box9 { grid-area: 7 / 9 / 10 / 13;border: 1px solid red;
+ } 
+.box10 { grid-area: 10 / 1 / 13 / 5;border: 1px solid red;
+ } 
+.box11 { grid-area: 10 / 5 / 13 / 9; border: 1px solid red;
+} 
+.box12 { grid-area: 10 / 9 / 13 / 13; border: 1px solid red;
+} 
 .titel { 
   grid-area: 4 / 5 / 7 / 9; 
   font-family:"" ;
   font-size: 73px;
-  color: white;
+  color: black;
   } 
 </style>
 
