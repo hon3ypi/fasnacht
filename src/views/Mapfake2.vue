daccc06ae10c387a61d317c230bf1b2f07923416
hon3ypi <wanda.lao@gmail.com>
2020-11-20 00:01:09 +0100

    convo mit grende pro karte

--- src/views/Mapfake2.vue
+++ src/views/Mapfake2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grendtest">
+  <div class="rathauskarte">
     <div ref="container" class="map"></div>
   </div>
 </template>
@@ -10,6 +10,7 @@ import mapboxgl from "mapbox-gl";
 import contentfulClient from "@/module/contentful.js";
 import fritschi from "../assets/fritschimarker.png";
 import grend from "../assets/grendmarker.png";
+import huereaff from "../assets/huereaffmarkersw.png";
 
 //Stuff woni noni wörkli chegge
 export default {
@@ -79,18 +80,38 @@ export default {
         },
       });
 
-      /*****************************************************/
-      let resultfritschi = await contentfulClient.getEntries({
-        content_type: "grende",
-      });
+      /*********************************************************** */
 
-      console.log(resultfritschi.item);
-      //let fritschi = resultfritschi.items;
+      map.addSource("rathausjesuiten", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          geometry: {
+            type: "LineString",
+            coordinates: [
+              [8.30591, 47.051748],
+              [8.306173, 47.050865],
+              [8.305379, 47.050838],
+              [8.304574, 47.050684],
+            ],
+          },
+        },
+      });
+      map.addLayer({
+        id: "rathausjesuiten",
+        type: "line",
+        source: "rathausjesuiten",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "grey",
+          "line-width": 8,
+        },
+      });
       /* FRITSCHIBURNNEN ************************************************************/
       map.loadImage(
-        //fritschi.fields.grendmedia.fields.file.url,
-        //"images.ctfassets.net/857folb0vp61/syTDePTB1SToGmTNhXyqI/3249e80243bdce19b4326cf8cd08ac0e/fritschimarker.png",
-        //"@/assets/fritschimarker.png",
         fritschi,
         function (error, image) {
           if (error) throw error;
@@ -114,7 +135,7 @@ export default {
                   },
                   properties: {
                     title: coordinates[3].fields.ortsname,
-                    description: '<a href="../#/grend1">Ech verzell der öppis dröbert...</a>',
+                    description: 'wotsch scho weder <a href="../#/grend1">zrogg</a> ?',
                   },
                 },
               ],
@@ -159,7 +180,7 @@ export default {
                 },
                 properties: {
                   title: coordinates[1].fields.ortsname,
-                  description: '<a href="../#/grend2">Ech verzell der öppis dröbert...</a>',
+                  description: 'die gugge, wo do speled hend voll die coole <a href="../#/grend2">grende</a>!',
                 },
               },
             ],
@@ -180,7 +201,79 @@ export default {
           },
         });
       });
-      
+      /* JESUITENPLATZ ************************************************************/
+      map.loadImage(huereaff, function (error, image) {
+        if (error) throw error;
+        map.addImage("huereaff", image);
+
+        //Point in Map
+        map.addSource("point3", {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                //Jesuitenplatz
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [
+                    coordinates[2].fields.location.lon,
+                    coordinates[2].fields.location.lat,
+                  ],
+                },
+                properties: {
+                  title: coordinates[2].fields.ortsname,
+                  description: 'du besch ez chli fitter för d fasnacht, aber besch du scho rüüdig am fiire? <a href="../#/grend3"></a>',
+                },
+              },
+            ],
+          },
+        });
+        // Add a symbol layer
+        map.addLayer({
+          id: "point3",
+          type: "symbol",
+          source: "point3",
+          layout: {
+            "icon-image": "huereaff",
+            // get the title name from the source's "title" property
+            "text-field": ["get", "title"],
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 1.25],
+            "text-anchor": "top",
+          },
+        });
+      });
+
+      /***********************************************************************************/
+      map.on("click", "point3", function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
+        console.log("en String");
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on("mouseenter", "point3", function () {
+        map.getCanvas().style.cursor = "pointer";
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on("mouseleave", "point3", function () {
+        map.getCanvas().style.cursor = "";
+      });
 
       /***********************************************************************************/
       map.on("click", "point2", function (e) {
@@ -251,14 +344,21 @@ export default {
 @import url("https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css");
 .map {
   float: left;
-  height: 100%;
+  height: 50%;
   width: 100%;
   position: fixed;
   left: 0;
 }
+
+a
+{
+color: #ED5250;
+text-decoration: underline;
+font-weight:bold;
+}
 .mapboxgl-popup {
   min-width: 200px;
-  max-width: 400px;
+  max-width: 200px;
   font: 12px/20px "Helvetica Neue", Arial, Helvetica, sans-serif;
 }
 </style>
