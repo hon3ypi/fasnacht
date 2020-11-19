b757b6418d1c5dddb2a1639086ae11ea8892eda7
Helena <mariahelena.brantschen@stud.hslu.ch>
2020-11-19 12:03:29 +0100

    home error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,56 @@
 <template class="">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
-
-  <div class="home">
+<div class="home">
+      <div class="boxen">
+    <div class="box1"></div>
+    <div class="box2"></div>
+    <div class="box3"></div>
+    <div class="box4"></div>
+    <div class="box4"></div>
+    <div class="box5"></div>
+    <div class="box6"></div>
+    <div class="box7"></div>
+    <div class="box8"></div>
+    <div class="box9"></div>
+    <div class="box10"></div>
+    <div class="box11"></div>
+    <div class="box12"></div>
+  </div>
+    <p class="titel">Luzerner Fasnchat</p>
   </div>
 </template>
 
 -->DAS ESCH FÖR S CSS
 <style>
-.html{
+html{
   font-family: 'Noto Sans', sans-serif;
 }
+
+.boxen { 
+display: grid; 
+grid-template-columns: repeat(12, 1fr); 
+grid-template-rows: repeat(12, 1fr); 
+grid-column-gap: 0px;
+grid-row-gap: 0px; 
+}
+.box1 { grid-area: 1 / 1 / 4 / 5; } 
+.box2 { grid-area: 1 / 5 / 4 / 9; } 
+.box3 { grid-area: 1 / 9 / 4 / 13; } 
+.box4 { grid-area: 4 / 1 / 7 / 5; } 
+.box5 { grid-area: 4 / 5 / 7 / 9; } 
+.box6 { grid-area: 4 / 9 / 7 / 13; } 
+.box7 { grid-area: 7 / 1 / 10 / 5; } 
+.box8 { grid-area: 7 / 5 / 10 / 9; } 
+.box9 { grid-area: 7 / 9 / 10 / 13; } 
+.box10 { grid-area: 10 / 1 / 13 / 5; } 
+.box11 { grid-area: 10 / 5 / 13 / 9; } 
+.box12 { grid-area: 10 / 9 / 13 / 13; } 
+.titel { 
+  grid-area: 4 / 5 / 7 / 9; 
+  font-family:"" ;
+  font-size: 73px;
+  color: white;
+  } 
 </style>
 
 -->DAS ESCH FÖR S JAVASCRIPT
