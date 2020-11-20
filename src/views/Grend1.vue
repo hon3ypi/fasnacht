1363383ebaffb38960b44ac6b504441ab40e583c
michellespengler <michellespengler@users.noreply.github.com>
2020-11-20 10:35:30 +0100

    :)

--- src/views/Grend1.vue
+++ src/views/Grend1.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template class=""> <div class="grend1 ">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link rel="stylesheet" href="reset.css">
@@ -36,64 +35,13 @@
 <img class="fritschi_bild_3" src="../assets/fritschi_3.jpg">
 </div>
   </div>
-=======
-<template class="">
-    <div class="grend1 ">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link rel="stylesheet" href="reset.css">
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
-
-        <section class="parent">
-            <p class="child1"><span class="blue">DU</span> hast <span class="blue">Bruder<br> Fritschi</span> gewählt
-            </p>
-            <img src="../assets/BruderFritschi.svg" class="child2">
-            <h4 class="child3 blue">Bru<br>der <br>Frit<br>schi</h4>
-            <span class="child4">
-                <h5 class="para-title">Gute Wahl</h5>
-                <p class="para-text"> Unübersehbar. Mit ehrfurchtgebietender gefurchter Holzmaske, langem Bart und im
-                    blauweissen Luzerner Staatsmantel. Einige sehen in ihm die mythische Symbolfigur eines heidnischen
-                    Fruchtbarkeitskults. Wahrscheinlicher ist, dass sein Vorbild ein «trinkfreudiger» Luzerner
-                    Landsknecht namens Fridolin (Friedrich, Fritschi) ist, der in der Schlacht von Ragaz 1446 am 6. März
-                    (Fridolinstag) beim Sieg der Eidgenossen über die Österreicher mitkämpfte.
-                    Heute eröffnet er mit der Fritschifamilie am Schmudo die Fasnacht. Folge ihm. Er wird dir Orte
-                    zeigen, die du noch nicht kennst. </p>
-            </span>
-        </section>
-
-        <section class="tagwach parent2">
-            <h4 class="yellow child5">tagwach</h4>
-            <h6 class="child7">Kapellplatz, SchmuDO 5:00 Uhr</h6>
-            <div class="wrapper-video yellowbg child6">
-                <video class="tagwach_video">
-                    <source
-                        src="/Users/michellespengler/Desktop/HSLU/3. Semester/Web und Mobile/fasnacht/src/assets/Bruele.mp4">
-                    </video>
-            </div>
-            <!--------------------- videos gehen nicht --------------------->
-            <video class="tagwach_video" controls>
-                <source src="assets/Urknall_Foetzelirege.mp4">
-            </video>
-            <p class="child12">1-0-0- t-a-u-s-e-n-d-e- P-a-p-i-e-r- s-c-h-n-i-t-z-e-l z-e-r-s-c-h-n-i-t-t-e-n-e
-                T-e-l-e-f-o-n-b-ü-c-h-e-r-n-
-                r-e-g-n-e-t-e-s-a-n-d-e-r-T-a-g-w-a-c-h-e-a-u-f-d-e-n-K-a-p-e-l-l-p-l-a-t-z-h-i-n-u-n-t-e-r-.
-            </p>
-            <img class="" src="">
-        </section>
-        <!--------------------- 3 Bilder am schluss --------------------->
-        <div class="fritschi_bilder">
-            <img class="fritschi_bild_1" src="../assets/fritschi_1.jpg">
-            <img class="fritschi_bild_2" src="../assets/fritschi_2.jpg">
-            <img class="fritschi_bild_3" src="../assets/fritschi_3.jpg">
-        </div>
-        <div class="footer">zurück zur Karte</div>
-    </div>
->>>>>>> a8c41dbafd31fa3c0ad1e2b0933e31927d6a30b2
 </template>
 
 -->DAS ESCH FÖR S CSS
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap");
-    @import url("reset.css");
+
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap") ;
+@import url("reset.css");
 
     /*color*/
     .blue {
@@ -130,7 +78,7 @@
 
     h4 {
         text-align: left;
-        font-size: 3vw;
+        font-size: 4em;
         text-transform: uppercase;
         line-height: 110%;
         font-weight: 800;
@@ -139,22 +87,26 @@
 
     h5 {
         text-align: left;
-        font-size: 6vw;
+        font-size: 2em;
         font-weight: 900;
         text-align: start;
         margin-bottom: 0%;
         margin-top: 5%;
-
     }
+
     h6 {
         text-align: left;
         padding-top: 5%;
         padding-left: 3%;
-        font-size: 2vw;
-        color:white;
+        font-size: 2em;
     }
 
-
+    p {
+        text-align: left;
+        margin-top: 1%;
+        font-size: 1.2em;
+        font-weight: 600;
+    }
 
     /* grid*/
     .parent {
@@ -164,22 +116,15 @@
         height: auto;
         grid-template-columns: repeat(12, 8.33vw);
         grid-template-rows: repeat(5, 8.33vw);
+        /*just for onepage ... how we do over multiple pages? just add more rows */
         grid-column-gap: 0px;
         grid-row-gap: 0px;
         overflow: hidden;
     }
-<<<<<<< HEAD
 .child5 { 
     grid-area: 2 / 2 / 4 / 6; 
     margin-top:0;
     align-self: flex-end;
-=======
-
-    .child1 {
-        grid-area: 2 / 2 / 3 / 6;
-        border: solid 1px blue;
-        font-size: 2em;
->>>>>>> a8c41dbafd31fa3c0ad1e2b0933e31927d6a30b2
     }
 
     .child2 {
@@ -193,29 +138,12 @@
         grid-area: 2 / 9 / 7 / 13;
         border: solid 1px blue;
         margin-top: 4.15vw;
-        font-size: 12vw;
-        font-weight: 600;
+        font-size: 7em;
     }
-.child7 { 
-    grid-area: 5 / 2 / 8 / 6; 
-    color: white;
-    margin:0;
-    padding:0;}
 
     .child4 {
-        grid-area: 7 / 2 / 13 / 6;
+        grid-area: 7 / 2 / 13 / 5;
         border: solid 1px blue;
-    }
-
-    .para-title {
-        font-size: 3vw;
-        font-weight: 600;
-    }
-
-    .para-text {
-        font-size: 2.2vw;
-        text-align: left;;
-
     }
 
     .parent2 {
@@ -230,7 +158,6 @@
         grid-row-gap: 0px;
     }
 
-<<<<<<< HEAD
 .fritschi_bild_1 {
     position: absolute;
     width:20%;
@@ -262,22 +189,6 @@
 .tagwach_video {
     widows: 640px;;
     height:360px;
-=======
-    .child5 {
-        grid-area: 2 / 2 / 4 / 6;
-        margin-top: 0;
-        align-self: flex-end;
-        font-size: 12vw;
-        font-weight: 600;
-
-    }
-
-    .child6 {
-        margin: 0;
-        padding: 0;
-        grid-area: 6 / 1 / 13 / 13;
-        min-height: 56vw;
->>>>>>> a8c41dbafd31fa3c0ad1e2b0933e31927d6a30b2
     }
 
     .child7 {
@@ -294,7 +205,7 @@
 
     .child12 {
         grid-area: 20 / 3 / 45 / 11;
-        font-size: 15.5vw;
+        font-size: 8.5em;
     }
 
     img {
@@ -308,38 +219,30 @@
         width: 100%;
         height: 100%
     }
-.s-vid {
-    width:100%;
-    height:100%
+
+    .s-vid {
+        width: 100%;
+        height: 100%
     }
 
-    .wrapper-video {
-        width: 100vw;
-        height: auto;
+    .wrapper-video {width:100vw;
+    height:auto;
+        
+    }
 
-    }
-    .footer{
-        width:100vw;
-        height:20vh;
-        color:#86DDB4;
-        font-size:2vw;
-        margin-top: 5%;
-    }
 </style>
 
 -->DAS ESCH FÖR S JAVASCRIPT
 <script>
-export default {
-  name: "Fritschi",
-  data: function () {
-    return {
-      ort: [],
-    };
-  },
-  mounted: async function () {
+    export default {
+        name: "Fritschi",
+        data: function () {
+            return {
+                ort: [],
+            };
+        },
+        mounted: async function () {
 
-  }
-}
-
+        }
+    }
 </script>
-
