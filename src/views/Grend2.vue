e0aa0b14216f73ae01ed8d00ce8bfd90f2baf700
Helena <mariahelena.brantschen@stud.hslu.ch>
2020-11-20 16:31:45 +0100

    grend2_text

--- src/views/Grend2.vue
+++ src/views/Grend2.vue
@@ -1,27 +1,45 @@
 <template>
+
+<div class="parent1">
+    <img class="kid1" src="../assets/Grend.svg">
+
+      <p class="kid2">Der Grend do</p>
+      <p class="kid3">Dä, wo do verbi gloffe esch!</p>
+
+    </div>
+
+  
+
 <div class="grend2">   
 
 <video class="grend_video" controls><source src="../assets/grend_frauen.mp4"></video>
 
+
+
+
+
+
  <section class="grend">
+
+
+
+
      <section class="vid-wrapper">
-           <!-- <video class="grend_vid" src="assets/Rathaustreppen.mp4"></video> -->
-        <p class="dialog">Dä, wo do verbi gloffe esch!</p>
        
         <img class="" src="">
-        <p class="dialog">Do nomol!</p>
+        <p class="dialog001">Do nomol ene!</p>
          <br/>
         
-    <span class="dialog-wrap dialog"> <span class="">Das esch jo en cooli </span>
-        <span class="nono"> Larva!</span></span>
+    <span class="dialog002"> <span class="">Das esch jo en cooli </span>
+        <span class="txtthrough"> Larva!</span></span>
     <span class="esch"> esch</span>
     <span class="tabu"> Tabu!</span>
 
         <div class="nono-words">
-            <p class="nono">Waggis!</p>
-            <p class="nono">Cortège!!</p>
-            <p class="nono">Schnitzelbängg!</p>
-            <p class="nono">Morgenschtraich!!</p>
+            <p class="nono1">Waggis!</p>
+            <p class="nono2">Cortège!!</p>
+            <p class="nono3">Schnitzelbängg!</p>
+            <p class="nono4">Morgenschtraich!!</p>
         </div>
         <div class="grid_bilder">
             <img class="grend_1" src="../assets/grend_1.jpg">
@@ -33,15 +51,14 @@
         </div>
         <div class="layer">
             <img class="" src="">
-            <p class="dialog">Dä, wo do verbi gloffe esch!</p>
-            <p class="dialog">Ha grad es Déjà Vue.</p>
+            <p class="dialog003">Dä, wo do verbi gloffe esch!</p>
+            <p class="dialog004">Ha grad es Déjà Vue.</p>
              <br/>
-            <p class="dialog">Das esch jo en coole Grend!</p>
+            <p class="dialog005">Das esch jo en coole Grend!</p>
              <br/>
         </div>
       
-            <h4>Guugge</h4>
-            <p>Rathaustreppe</p>
+            <p class="ort">Guugge bei der Rathaustreppe</p>
             <video class="grend_gugge" controls><source src="../assets/Rathaustreppen.mp4"></video>
             <img class="gugge_1" src="../assets/grend_gugge_1.jpg">
             <img class="gugge_2" src="../assets/grend_gugge_2.jpg">
@@ -97,18 +114,100 @@ font-size: 60px;
 text-align: center;
 color: black;
 text-transform: uppercase;
+text-decoration: ;
     }
 .nono-words {
 font-size: 100px;
+padding-left:8vw;
+padding-bottom:3vw;
 text-decoration-line: line-through;
 text-align: center;
 color: black;
     }
 
+ .txtthrough   {  text-decoration: line-through;}
+
+
+.parent1 {
+  display: grid;
+  font-size: 4.4vw;
+  text-align: left;
+  text-transform: uppercase;
+  width: 100vw;
+  height: auto;
+  grid-template-columns: repeat(12, 8.33vw);
+  grid-template-rows: repeat(12, 8.33vw);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+
+.kid1 {
+  grid-area: 1 / 6 / 5 / 12;
+  text-transform: uppercase;
+  font-weight: 800;
+  font-size: 4.4vw;
+  text-align: left;
+}
+
+.kid2 {
+  text-align: left;
+  font-weight: 800;
+  font-size:4.4vw;
+  grid-area: 2 / 2 / 4 / 3;
+}
+
+.kid3 {
+  grid-area: 6 /2 / 8 / 9;
+  font-weight: 800;
+    font-size:4.4vw;
+
+}
+.esch{
+    padding-right:8vw;
+    padding-left:69vw;
+}
+.tabu{
+    font-size:17.6vw;
+    text-align:right;
+    padding-right:8vw
+}
+
+
+.dialog001 , .dialog003{padding-left:8%;
+    padding-right:8%;
+    padding-top:8%;
+    font-size:4.4vw;
+        font-weight:800;
+
+    text-align:left;
+
+}
+.dialog002, .dialog004{padding-left:8%;
+    padding-right:8%;
+    padding-top:8%;
+    font-size:4.4vw;
+    font-weight:800;
+    text-align:right;
+
+}
+
+.dialog005{padding-left:8%;
+    padding-right:8%;
+    padding-top:8%;
+    font-size:4.4vw;
+        font-weight:800;
+
+    text-align:right;
+
+}
+
+
 .grend_frauen {
     width: 100%;
     height: 100%;
 }
+
+/* grid 2*/
 .grid_bilder {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
@@ -153,9 +252,14 @@ color: black;
 .treppen_text {
     text-align: left;
     margin-top: 10%;
-    margin-left: 10%;
-    font-size: 60%;
-    font-weight: 600;
+    padding-left: 8%;
+    font-weight: 400;
+    font-size:3.4vw;
+}
+.ort{margin-left:8vw;
+font-size:3.2vw;
+font-weight:600;
+color:#86DDB4
 }
 </style>
 
